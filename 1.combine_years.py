c5d7f5b4c513e55cd164a55677728e5200a62660
GL192021 <georgios.lamprinakis@gmail.com>
2026-01-23 20:09:02 +0200

    Fix path variable from main_path to RAW

--- 1.combine_years.py
+++ 1.combine_years.py
@@ -10,14 +10,14 @@ OUT.mkdir(parents=True, exist_ok=True)
 years = []
 combined_data = {}
 s=0
-for fname in os.listdir(main_path):
+for fname in os.listdir(RAW):
     if fname.lower().startswith('atp_matches'):
         if not fname.lower().endswith("toy_example.csv"):
             match = re.search(r"atp_matches_(\d{4})", fname)
             if match:
                 year = int(match.group(1))
                 years.append(year)
-                atp_y_path = os.path.join(main_path, f"atp_matches_{year}.csv")
+                atp_y_path = os.path.join(RAW, f"atp_matches_{year}.csv")
                 atp_y = pd.read_csv(atp_y_path)
                 s += len(atp_y)
                 columns_lst = atp_y.columns
@@ -29,7 +29,7 @@ for fname in os.listdir(main_path):
 start_year = min(years)
 end_year = max(years)
 
-pd.DataFrame(combined_data).to_csv(os.path.join(output_path, f"combined_{start_year}-{end_year}.csv"), index=False)
+pd.DataFrame(combined_data).to_csv(os.path.join(OUT, f"combined_{start_year}-{end_year}.csv"), index=False)
 
 
 
